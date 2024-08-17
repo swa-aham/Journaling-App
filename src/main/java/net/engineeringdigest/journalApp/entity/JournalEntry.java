a88bff3979e95daeb1703d3ad49f9f6958d41942
Swa-aham <sohammandaviya@gmail.com>
2024-08-18 02:20:18 +0530

    journalEntry class modified for mongo

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,16 +1,35 @@
 package net.engineeringdigest.journalApp.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "journal_entries")
 public class JournalEntry {
 
-    private long id;
+    @Id
+    private String id;
+
     private String title;
+
     private String content;
 
-    public long getId() {
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
