29c51288603a92fee2fa2c6e43a953a73b62e4ba
Swa-aham <sohammandaviya@gmail.com>
2024-12-07 02:05:55 +0530

    Secured the admin endpoint using roles

--- src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
+++ src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
@@ -25,6 +25,7 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
         http.authorizeHttpRequests()
                 .antMatchers("/journal/**", "/user/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
