4c3ba11f057bf5182b3ef3907eaf3d185fabb7b4
Swa-aham <sohammandaviya@gmail.com>
2024-12-28 02:22:31 +0530

    Weather service for API usage with RestTemplate

---
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -0,0 +1,27 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.api.response.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+
+    private static final String API_KEY = "adc2b7ba9f0c3f944eaf487e2eb95098";
+
+    private final String URL = "http://api.weatherstack.com/current?access_key=KEY&query=CITY";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    public WeatherResponse getTemp(String city) {
+        String endPoint = URL.replace("CITY", city).replace("KEY", API_KEY);
+        ResponseEntity<WeatherResponse> response = restTemplate.exchange(endPoint, HttpMethod.GET, null, WeatherResponse.class);
+        WeatherResponse body = response.getBody();
+        return body;
+    }
+}
