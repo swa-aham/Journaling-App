fc896a16c646d498488443296084fde3f2afb27a
Swa-aham <sohammandaviya@gmail.com>
2024-12-30 02:03:41 +0530

    Testcase for verification

---
+++ src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImplTest.java
@@ -0,0 +1,17 @@
+package net.engineeringdigest.journalApp.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserRepositoryImplTest {
+
+    @Autowired
+    private UserRepositoryImpl userRepositoryImpl;
+
+    @Test
+    public void getUserForSATest() {
+        userRepositoryImpl.getUserForSA();
+    }
+}
