cfbe14c43ea75cb1c281afe9325817950b317a44
Swa-aham <sohammandaviya@gmail.com>
2024-12-31 00:48:57 +0530

    Email details

---
+++ src/main/java/net/engineeringdigest/journalApp/entity/EmailDetails.java
@@ -0,0 +1,15 @@
+package net.engineeringdigest.journalApp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDetails {
+
+    private String recipient;
+    private String msgBody;
+    private String subject;
+}
