616e4cc5ac398359a9eb9636ed469942ffdee8cf
Swa-aham <sohammandaviya@gmail.com>
2025-01-28 05:34:22 +0530

    service class for redis services

---
+++ src/main/java/net/engineeringdigest/journalApp/service/RedisService.java
@@ -0,0 +1,44 @@
+package net.engineeringdigest.journalApp.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import net.engineeringdigest.journalApp.api.response.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.TimeoutUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
+public class RedisService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public <T> T get(String key, Class<T> entityClass) {
+        try {
+            Object object = redisTemplate.opsForValue().get(key);
+//            if (object == null) {
+//                return null;
+//            }
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(object.toString(), entityClass);
+        } catch (Exception e) {
+            log.error("Exception in get ", e);
+            return null;
+        }
+    }
+
+    public void set(String key, Object o, Long ttl) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonValue = objectMapper.writeValueAsString(o);
+            redisTemplate.opsForValue().set(key, jsonValue, ttl, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            log.error("Exception int setter", e);
+        }
+    }
+}
